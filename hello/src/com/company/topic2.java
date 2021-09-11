bd911917c106dfbcc071409d4101e6e5da98bdb5
林新钊 <2059205232@qq.com>
2021-09-11 16:47:25 +0800

    作业

---
+++ hello/src/com/company/topic2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class topic2 {
+    public static void main(String[] args) {
+        String str1 ="a,b,s,sd,Absdf,asdasdd,a";
+        int[] count = new int[52];
+        for (int i = 0; i < str1.length(); i++) {
+            char tmp = str1.charAt(i);
+            if ((tmp >= 65 && tmp < 90) || (tmp >= 97 && tmp < 122)) {
+                int index = tmp - 65;
+                count[index] = count[index] + 1;
+            }
+        }
+        for (int j = 0; j < count.length; j++) {
+            if (count[j] != 0)
+                System.out.println("字母" + (char) (j + 65) + "出现次数" + count[j]);
+        }
+    }
+}
