a0eb9a323e0457d0e6a96760b47f8c6f4afe68a9
林新钊 <2059205232@qq.com>
2021-09-13 12:10:58 +0800

    作业

---
+++ hello/src/com/company/Triangle.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        double a,b,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入三角形的三边：");
+        a= sc.nextDouble();
+        b= sc.nextDouble();
+        c= sc.nextDouble();
+        testTriangle angle=new testTriangle(a,b,c);
+        if(angle.istriangle()){
+            System.out.println("周长"+angle.peretriangle());
+            System.out.println("面积"+angle.areatriangle());
+        }else  System.out.println("错误");
+    }
+}
+class testTriangle{
+    private double a,b,c;
+    public testTriangle(double a,double b,double c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    public boolean istriangle(){
+        if((a+b)>c&&(a+c>b)&&(b+c)>a){
+            return true;
+        }
+        else return false;
+    }
+    public double peretriangle(){
+        return a + b + c;
+    }
+
+    public double areatriangle(){
+        double area=0.0,temp;
+        temp=0.5*(a+b+c);
+        area= Math.sqrt(temp*(temp-a)*(temp-b)*(temp-c));
+        return area;
+    }
+}
+
+
